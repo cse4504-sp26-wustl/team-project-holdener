62032745e4ce9e3a2c123c7c3644859be27f3ba4
Kate Holdener <kate.holdener@gmail.com>
2026-02-18 09:18:09 -0600

    updated pairing algorithm

--- domain/player.py
+++ domain/player.py
@@ -1,11 +1,11 @@
 
 from typing import Optional
 class Player:
-    def __init__(self, firstName: str, lastName: str, uscfId: str, rating=Optional[int]):
+    def __init__(self, firstName: str, lastName: str, uscfId: str, rating=None):
         self.firstName = firstName
         self.lastName  = lastName
         self.id = uscfId
-        self.rating = None
+        self.rating = int(rating) if rating else 0
 
     def to_dict(self):
         dict_obj = self.__dict__.copy()
